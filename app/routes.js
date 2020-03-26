14624cc146486e73a86242c6a0b46c37e6af236b
gazjoy <post@garethjoyce.co.uk>
2020-03-26 16:11:57 +0000

    [Journey] sign-up sign-in setup

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,12 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
+router.post('/email', function (req, res) {
+    res.redirect('check-your-email');
+})
+
+router.post('/sign-in', function (req, res) {
+    res.redirect('messages');
+})
+
 module.exports = router;
