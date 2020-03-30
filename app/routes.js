2658aa5c7879e4247ef85dc0583b64c3208a0bd0
gazjoy <post@garethjoyce.co.uk>
2020-03-30 14:23:48 +0100

    [Journeys] Fixed up journeys to align with Miro

--- app/routes.js
+++ app/routes.js
@@ -4,15 +4,9 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 
-router.post('/email', function (req, res) {
-    res.redirect('check-your-email');
-})
-
-router.post('/sign-in', function (req, res) {
-    res.redirect('messages');
-})
 
 // This moves routing to another directory
+router.use('/1/', require('./views/1/_routes'))
 router.use('/2/', require('./views/2/_routes'))
 router.use('/3/', require('./views/3/_routes'))
 router.use('/4/', require('./views/4/_routes'))
--- app/views/1/_routes.js
+++ app/views/1/_routes.js
@@ -3,15 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 // Add your routes here - above the module.exports line
-
-
-router.post('/code', function (req, res) {
-    res.redirect('messages');
-})
-
-
 router.post('/email', function (req, res) {
     res.redirect('check-your-email');
+})
+
+router.post('/sign-in', function (req, res) {
+    res.redirect('messages');
 })
 
 module.exports = router;
